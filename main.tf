1d0640ea5f822728ed7a2683034fad7dde1ca291
uturunku1 <luces.huayhuaca@gmail.com>
2023-04-13 16:01:14 -0700

    I am out of apply quota limit. Apply will not run because I am increasing RUM count

--- main.tf
+++ main.tf
@@ -27,28 +27,28 @@ variable "username" {
   default = "uturunku1"
 }
 
-# resource "random_pet" "always_new_name" {
-#   keepers = {
-#     uuid = uuid() # Force a new name each time
-#   }
-#   length = 3
-# }
+resource "random_pet" "always_new_name" {
+  keepers = {
+    uuid = uuid() # Force a new name each time
+  }
+  length = 3
+}
 
-# resource "random_uuid" "test" {
-# }
-# resource "random_uuid" "test2" {
-# }
+resource "random_uuid" "test" {
+}
+resource "random_uuid" "test2" {
+}
 
-# resource "null_resource" "username" {
-#   triggers = {
-#     # Generate a new username each time we switch the value of the variable username
-#     username = var.username
-#   }
-# }
+resource "null_resource" "username" {
+  triggers = {
+    # Generate a new username each time we switch the value of the variable username
+    username = var.username
+  }
+}
 
-# resource "null_resource" "random_uuid" {
-#   triggers = {
-#     # Generate a new username each time we switch the value of the variable username
-#     username = "${random_uuid.test.result}-rg"
-#   }
-# }
+resource "null_resource" "random_uuid" {
+  triggers = {
+    # Generate a new username each time we switch the value of the variable username
+    username = "${random_uuid.test.result}-rg"
+  }
+}
