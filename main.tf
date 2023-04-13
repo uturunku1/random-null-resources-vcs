6e162200fde03ec8200828fc295a69cb9ad1d801
uturunku1 <luces.huayhuaca@gmail.com>
2023-04-13 15:56:28 -0700

    I am out of apply quota limit but apply will run because I am reducing my RUM

--- main.tf
+++ main.tf
@@ -27,24 +27,24 @@ variable "username" {
   default = "uturunku1"
 }
 
-resource "random_pet" "always_new_name" {
-  keepers = {
-    uuid = uuid() # Force a new name each time
-  }
-  length = 3
-}
+# resource "random_pet" "always_new_name" {
+#   keepers = {
+#     uuid = uuid() # Force a new name each time
+#   }
+#   length = 3
+# }
 
-resource "random_uuid" "test" {
-}
-resource "random_uuid" "test2" {
-}
+# resource "random_uuid" "test" {
+# }
+# resource "random_uuid" "test2" {
+# }
 
-resource "null_resource" "username" {
-  triggers = {
-    # Generate a new username each time we switch the value of the variable username
-    username = var.username
-  }
-}
+# resource "null_resource" "username" {
+#   triggers = {
+#     # Generate a new username each time we switch the value of the variable username
+#     username = var.username
+#   }
+# }
 
 # resource "null_resource" "random_uuid" {
 #   triggers = {
