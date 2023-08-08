70cd069fc558a76f18b6468259e128de4fc251da
uturunku1 <luces.huayhuaca@gmail.com>
2023-08-08 16:48:31 -0700

    add a new resource, number 4

--- main.tf
+++ main.tf
@@ -42,6 +42,9 @@ resource "random_uuid" "test2" {
 resource "random_uuid" "test3" {
 }
 
+resource "random_uuid" "test4" {
+}
+
 resource "null_resource" "username" {
   triggers = {
     # Generate a new username each time we switch the value of the variable username
